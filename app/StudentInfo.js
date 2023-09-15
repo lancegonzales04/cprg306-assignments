6559a5d5120b79e08fbbc628267fc16c1f93cace
lancegonzales04 <119756934+lancegonzales04@users.noreply.github.com>
2023-09-15 13:37:32 -0600

    week2

---
+++ app/StudentInfo.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <div>
+          <p> Name: Lance Gonzales</p>
+          <p> Course Section: CPRG 306 A </p>
+          <p><a href="https://github.com/lancegonzales04/cprg306-assignments"> https://github.com/lancegonzales04/cprg306-assignments</a></p>
+        </div>
+        </main>
+  );
+}
---
+++ app/week2/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link" 
+import StudentInfo from "../StudentInfo"
+
+export default function Home() {
+    return (
+    <main>
+    <h1> MY SHOPPING LIST</h1>
+    
+    <StudentInfo/>
+    </main>
+    )
+}
+
